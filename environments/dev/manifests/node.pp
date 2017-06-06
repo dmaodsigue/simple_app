eb994196ada1244b24eb41614edef308ca84392b
Dave Michael Odsigue <dave.odsigue@orbium.com>
2017-06-06 09:51:12 +0800

    Install vim package

--- environments/dev/manifests/node.pp
+++ environments/dev/manifests/node.pp
@@ -9,17 +9,13 @@ node default {
   apache::vhost {'dave.com':
     port    => 80,
     docroot => '/vagrant/app',
-    #custom_fragment => 'AddType application/x-httpd-php .php',
   }
-  #file { 'puppet.conf':
-  #  path   => '/etc/puppetlabs/puppet/puppet.conf',
-  #  ensure => file,
-  #  content => template('puppet/puppet.conf.erb'),
-  #}
   firewall { '000 accept port 80/tcp':
     dport  => [80],
     proto  => tcp,
     action => accept,
-  #  require => File ['puppet.conf'],
+  }
+  package { 'vim':
+    ensure => 'installed'
   }
 }
